5a72e7d29b01506ececa927f003381b61e159c31
Cameron McCosh <2242033m@student.gla.ac.uk>
2018-01-26 14:14:45 +0000

    2nd commit

---
+++ rango/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Rango says hey there partner!</br><a href=\'/rango/about/\'>About</a>')
+
+def about(response):
+    return HttpResponse('Rango says here is the about page.</br><a href=\'/rango/\'>Index</a>')
