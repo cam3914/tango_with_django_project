d762fdae43560372dcf682bcb6a9e97146f8d180
Cameron McCosh <2242033m@student.gla.ac.uk>
2018-02-07 04:58:09 +0000

    Chapter 4

--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Rango says hey there partner!</br><a href=\'/rango/about/\'>About</a>')
+    # Construct a dictionary to pass to the template engine as its context
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    # Return  a rendered response to send to the client
+    return render(request, 'rango/index.html', context=context_dict)
 
-def about(response):
-    return HttpResponse('Rango says here is the about page.</br><a href=\'/rango/\'>Index</a>')
+def about(request):
+    # Construct a dictionary to pass to the template engine as its context
+    context_dict = {'excerciseline': "This tutorial has been put together by Cameron McCosh."}
+    # Return  a rendered response to send to the client
+    return render(request, 'rango/about.html', context=context_dict)
